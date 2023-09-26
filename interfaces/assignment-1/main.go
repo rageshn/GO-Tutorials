9edd55d25f9511340d615f088b212854effc0bdd
Ragesh <ragesh.narayanan@gmail.com>
2023-09-26 21:27:40 +0530

    Added assignments

---
+++ interfaces/assignment-1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	tr := triangle{
+		height: 10,
+		base:   15,
+	}
+	sq := square{
+		sideLength: 10,
+	}
+
+	printArea(tr)
+	printArea(sq)
+}
